540b360c762be3e925360943654214682feaf37e
lwangenheim <lwangenheim@gmail.com>
2014-05-26 21:45:34 -0400

    db setup

--- server/config.js
+++ server/config.js
@@ -3,10 +3,10 @@ path = require('path');
 module.exports = {
   mongo: {
     dbUrl: 'https://api.mongolab.com/api/1',            // The base url of the MongoLab DB server
-    apiKey: '4fb51e55e4b02e56a67b0b66'                 // Our MongoLab API key
+    apiKey: 'kDwSSqsIF56OESQo3rLahlNqP56oC_7-'                 // Our MongoLab API key
   },
   security: {
-    dbName: 'ascrum',                                   // The name of database that contains the security information
+    dbName: 'lwangenheim-angular-app',                                   // The name of database that contains the security information
     usersCollection: 'users'                            // The name of the collection contains user information
   },
   server: {
